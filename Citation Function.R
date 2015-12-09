1647b95388541ed595c5fb18cc116b76d0a2a073
Hunter Ratliff <hunterratliff1@gmail.com>
2015-12-09 14:48:42 -0600

    Minor changes

--- Citation Function.R
+++ Citation Function.R
@@ -1,27 +1,27 @@
-citation = function (text="@HunterRatliff1", pos="BR", dist=0.02,
-                     font.size=12, color="#333F48", fontfamily="", style="plain", 
+citation <- function (text="@HunterRatliff1", pos="BR", dist=0.02,
+                     font.size=12, color="#333F48", fontfamily="", style="plain",
                      draw=TRUE){
   ## Required packages
-  sapply(c("grid", "gridExtra", "stringi", "lubridate"), require, character.only=TRUE)  
-  
+  sapply(c("grid", "gridExtra", "stringi", "lubridate"), require, character.only=TRUE)
+
   if(stri_startswith_fixed(pos, "B")) Y = unit(dist, "npc")   # Bottom
   if(stri_startswith_fixed(pos, "T")) Y = unit(1-dist, "npc") # Top
   if(stri_endswith_fixed(pos,   "R")) X = unit(1-dist, "npc") # Right
   if(stri_endswith_fixed(pos,   "L")) X = unit(dist, "npc")   # Left
-  
+
   if(stri_endswith_fixed(pos, "R") & stri_startswith_fixed(pos, "B")) Just=c("right", "bottom")
   if(stri_endswith_fixed(pos, "L") & stri_startswith_fixed(pos, "B")) Just=c("left",  "bottom")
   if(stri_endswith_fixed(pos, "R") & stri_startswith_fixed(pos, "T")) Just=c("right", "top")
   if(stri_endswith_fixed(pos, "L") & stri_startswith_fixed(pos, "T")) Just=c("left",  "top")
-  
+
   GP = gpar(
-    fontsize   = font.size, 
+    fontsize   = font.size,
     col        = color,
     fontfamily = fontfamily,
-    fontface   = style) 
-  
+    fontface   = style)
+
   text.grob <- textGrob(as.character(text), x=X, y=Y, just=Just, gp=GP)
-  
+
   if(draw){
     grid.draw(text.grob)
   } else {
@@ -31,28 +31,28 @@ citation = function (text="@HunterRatliff1", pos="BR", dist=0.02,
 
 
 citation.date <- function(text="@HunterRatliff1", draw=TRUE) {
-  sapply(c("grid", "gridExtra", "stringi", "lubridate"), require, character.only=TRUE)  
-  
+  sapply(c("grid", "gridExtra", "stringi", "lubridate"), require, character.only=TRUE)
+
   Date <- as.character(today())
   Text <- paste("Created on", Date)
-  
+
   citation(text = Text, pos = "BL", color = "#382F2D", dist = 0.005, draw = draw)
-  citation(text=text, pos = "TR", color = "#333F48", fontfamily = "Open Sans")
+  citation(text = text, pos = "TR", color = "#333F48", fontfamily = "Open Sans")
 }
- 
+
 
 
 
 
 citation.apa = function (text="", draw=TRUE) {
   ## Required packages
-  sapply(c("grid", "gridExtra", "stringi", "lubridate"), require, character.only=TRUE)  
-  #   
+  sapply(c("grid", "gridExtra", "stringi", "lubridate"), require, character.only=TRUE)
+  #
   # Orange  :: #F05B49
   # Green   :: #6299A0
   # DarkGrn :: #007371
   # LigtGrn :: #C2E8EB
-  # 
+  #
   citation(text = "hunter.ratliff@austinpetsalive.org", pos = "BR", dist = 0.005,
            font.size = 12, color = "#4D4D4D", fontfamily = "Verdana", style = "italic",
             draw = draw)
@@ -64,20 +64,23 @@ citation.apa = function (text="", draw=TRUE) {
   citation(text = paste("Created on", as.character(today())), pos = "BL", dist = 0.005,
            font.size = 13, color = "#DD5928", fontfamily = "Georgia",
            draw = draw)
-  
+
 }
 
 
-citation.htr = function (text="") {
+citation.htr = function (text="", color="#5BC002", twitter=TRUE) {
   ## Required packages
   suppressPackageStartupMessages(require(grid))
   suppressPackageStartupMessages(require(gridExtra))
   suppressPackageStartupMessages(require(stringi))
   suppressPackageStartupMessages(require(lubridate))
-  
-  grid.text(as.character("hunterratliff1@gmail.com"), x=unit(0.995, "npc"), y=unit(0.995, "npc"), just=c("right", "top"), 
+
+
+
+  grid.text(as.character(ifelse(twitter, "@HunterRatliff1", "hunterratliff1@gmail.com")),
+            x=unit(0.995, "npc"), y=unit(0.995, "npc"), just=c("right", "top"),
             gp=gpar(
-              fontface = "italic", 
+              fontface = "italic",
               family="Open Sans",
               col = "#333F48",
               cex=0.8)
@@ -85,13 +88,13 @@ citation.htr = function (text="") {
             # axis.title = element_text(family="Verdana", color = "#5BC002", face = "bold")
             # Georgia
   )
-  grid.text(as.character(text), x=unit(0.995, "npc"), y=unit(0.005, "npc"), just=c("right", "bottom"), 
+  grid.text(as.character(text), x=unit(0.995, "npc"), y=unit(0.005, "npc"), just=c("right", "bottom"),
             gp=gpar(
-              # fontface = "bold", 
+              # fontface = "bold",
               family="American Typewriter",
-              col = "#5BC002",
+              col = color,
               cex=1.15)
   )
-  grid.text(paste("Created on", as.character(today())), x=unit(0.005, "npc"), y=unit(0.005, "npc"), just=c("left", "bottom"), 
+  grid.text(paste("Created on", as.character(today())), x=unit(0.005, "npc"), y=unit(0.005, "npc"), just=c("left", "bottom"),
             gp=gpar(col = "#382F2D", cex=0.75))
 }
